9ed7838d122b4f3b0a5f65bee1560aca5a72ab6d
Beverly Potts <pottsbeverly@gmail.com>
2020-06-10 19:25:52 -0400

    Add 'way' to words beginning with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,9 @@
     charArray = input.split("");
     if ((charArray.length === 1) && ((input === "a") || (input === "A") || (input === "i") || (input === "I") || (input === "o") || (input === "O"))) {
       return input +"way";
-    } else {
-      return input;
+    } else if ((charArray.length > 1) && ((charArray[0] === "a")||(charArray[0] === "A")||(charArray[0] === "e")||(charArray[0] === "E")||(charArray[0] === "I")||(charArray[0]) === "i")||(charArray[0] === "o")||(charArray[0] === "O")||(charArray[0] === "u")||(charArray[0]=== "U"))
+      return input + "way";
     }
-  }
-   
-   
   $(document).ready(function() {
     $("form#form").submit(function(event) {
       event.preventDefault();
